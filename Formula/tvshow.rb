ebba951784f25f564231eaba39dc315bffa611ab
Doarakko <2wua4nlyi@gmail.com>
2026-01-10 02:42:16 +0900

    fix(formula): update tvshow version to 0.0.3 and correct SHA256 checksums

--- Formula/tvshow.rb
+++ Formula/tvshow.rb
@@ -1,25 +1,25 @@
 class Tvshow < Formula
   desc "Display Japanese TV schedules in the terminal"
   homepage "https://github.com/Doarakko/tvshow"
-  version "0.0.2"
+  version "0.0.3"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/Doarakko/tvshow/releases/download/v#{version}/tvshow-x86_64-apple-darwin.tar.gz"
-      sha256 "40849abc764b45160b953909287d626bbc54f40537154df2147baff11b08655a"
+      sha256 "4cf5503bf258b168286883405f7bdb0710f1ebf26abe5f59c30e330760d69b65"
     end
 
     on_arm do
       url "https://github.com/Doarakko/tvshow/releases/download/v#{version}/tvshow-aarch64-apple-darwin.tar.gz"
-      sha256 "e939f63aa63741472fe90d56e62a313a53643458970dac0f70e15638eba972cf"
+      sha256 "8dd33e953fe47177188721f4d6a514a446fae41b74f0954c751061b6dde974da"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/Doarakko/tvshow/releases/download/v#{version}/tvshow-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "a71ae0dc627bd57378c741066223470b35f0ca0ac30124edf6e3f1042f0e246d"
+      sha256 "a6930adf1d24a70a367a11d749211b977eed750427466be188fbeb25e97d3b0b"
     end
   end
 
